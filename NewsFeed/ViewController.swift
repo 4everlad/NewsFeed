a822a4981a79eafcc1eb05a6658bba7041cbf8bf
Dmitry Bakulin <d.s.bakulin@gmail.com>
2019-01-30 20:08:59 +0300

    Initial Commit

---
+++ NewsFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsFeed
+//
+//  Created by Dmitry Bakulin on 30/01/2019.
+//  Copyright © 2019 Dmitry Bakulin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
